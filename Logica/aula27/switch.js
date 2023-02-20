9cf3f71325860f3279c2e766fbe85d33842351ff
Fabricio Alves <fabricioallves2020@gmail.com>
2023-02-20 08:07:27 -0300

    switch case

--- Logica/aula27/switch.js
+++ Logica/aula27/switch.js
@@ -1,6 +1,35 @@
 // estrutura condicional switch case
 const data = new Date('1987-04-21 00:00:00');
-const diaSemana = data.getDay();
+let diaSemana = data.getDay();
+let diaSemanaTexto;
 
+// utilizando switch
+switch (diaSemana) {
+    case 0:
+        diaSemanaTexto = 'Domingo';
+        break;
+    case 1:
+        diaSemanaTexto = 'Segunda';
+        break;
+    case 2:
+        diaSemanaTexto = 'Terça';
+        break;
+    case 3:
+        diaSemanaTexto = 'Quarta';
+        break;
+    case 4:
+        diaSemanaTexto = 'Quinta';
+        break;
+    case 5:
+        diaSemanaTexto = 'Sexta';
+        break;
+    case 6:
+        diaSemanaTexto = 'Sábado';
+        break;
+    default:
+        diaSemanaTexto = 'Inválido';
+}
+
+console.log(diaSemana, diaSemanaTexto);
 
 
