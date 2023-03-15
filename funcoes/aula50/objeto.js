83312dfaf94ced86bbc5130e14c7fee178f9ceca
Fabricio Alves <fabricioallves2020@gmail.com>
2023-03-15 10:45:41 -0300

    função de construção de objetos

--- funcoes/aula50/objeto.js
+++ funcoes/aula50/objeto.js
@@ -1,13 +1,25 @@
 // funções fábricas
-function criaPessoa(nome, sobrenome) {
+function criaPessoa(nome, sobrenome, altura, peso) {
     return {
+        // atributos
         nome, sobrenome,
 
-        // método da função
-        fala: function (assunto) {
-            return `${nome} está ${assunto}`;
+        // método do objeto
+        fala(assunto) {
+            return `${this.nome} está ${assunto}`;
+        },
+
+        altura: altura,
+        peso: peso,
+
+        // método sem parâmetro
+        imc() {
+            const indice = this.peso / (this.altura ** 2);
+            return indice.toFixed(2);
         }
     };
 }
 
-const p1 = criaPessoa('Luiz', 'Otávio');
+const p1 = criaPessoa('Luiz', 'Otávio', 1.80, 80);
+console.log(p1.fala('falando sobre finanças.'));
+console.log(p1.imc());
