ff9a6caacfde6a1b6cc6bc43a686aebe8ad7d968
Fabricio Alves <fabricioallves2020@gmail.com>
2023-04-12 08:08:22 -0300

    validador de cpf

--- objetos-prototypes/aula73/modelo/assets/js/index.js
+++ objetos-prototypes/aula73/modelo/assets/js/index.js
@@ -3,8 +3,8 @@ function CPF () {
         cpf: document.querySelector('.text-cpf'),
 
         digitandoCpf() {
-            document.addEventListener('keydown', e => {
-                if(this.comparaTeclas(e.keyCode) && e.target.classList.contains('text-cpf')) {
+            this.cpf.addEventListener('keypress', e => {
+                if(this.comparaTeclas(event.keyCode)) {
                     this.inseriCaracter();
                 }
             })
@@ -28,30 +28,48 @@ function CPF () {
             }else if(this.cpf.value.length === 11){
                 this.cpf.value += '-';
             }else if(this.cpf.value.length === 14) {
-                this.eValido();
+                console.log(this.eValido());
             }
         },
 
         eValido() {
-            const numeroCpf = this.pegandoSoNumero(this.cpf);
-            const digitoUm = primeiroDigito(numeroCpf);
-            const digitoDois = segundodigito()
-
+            const numeroCpf = this.pegandoSoNumero(this.cpf.value.slice(0, this.cpf.length));
+            console.log(numeroCpf);
+            const digitoUm = this.primeiroDigito(numeroCpf.slice(0, numeroCpf.slice(0, numeroCpf.length)));
+            console.log(digitoUm);
+            const digitoDois = this.segundoDigito(numeroCpf.slice(0, numeroCpf.length));
+            return Number(numeroCpf[9]) === digitoUm && Number(numeroCpf[10]) === digitoDois ? 'cpf válido': 'cpf inválido';
         },
 
         pegandoSoNumero(cpf) {
-            const numero = cpf.value.replace(/\D/g, '');
-            return numero;
+            const numero = cpf.replace(/\D/g, '');
+            return numero.split('');
         },
 
         primeiroDigito(cpf) {
             cpf.pop();
             cpf.pop();
+            let total = 0;
+            let cont = 0;
+            let cont2 = 2;
+            console.log(cpf[cont2]);
+            while(cont < 9) {
+                total += Number(cpf[cont]) * cont2;
+                console.log(total);
+                cont++;
+                cont2++;
+            }
+            return 11 - (total % 11);
+        },
+
+        segundoDigito(cpf) {
+            cpf.pop();
+            cpf = Number(cpf);
             const cpf13 = cpf.filter(valor => valor);
             let total = 0;
             let cont = 11;
             let cont2 = 0;
-            while(cont > 2) {
+            while(cont > 1) {
                 total += cont * cont2;
                 cont--;
                 cont2++;
@@ -63,6 +81,7 @@ function CPF () {
 
 const cpf = CPF();
 cpf.digitandoCpf();
+
 
 
 
