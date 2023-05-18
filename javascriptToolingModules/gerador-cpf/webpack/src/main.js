70151f750d8ea8ba9098f70243eff19b9d872700
Fabricio Alves <fabricioallves2020@gmail.com>
2023-05-18 08:15:38 -0300

    update: basico
    Gerador de senha

--- javascriptToolingModules/gerador-cpf/webpack/src/main.js
+++ javascriptToolingModules/gerador-cpf/webpack/src/main.js
@@ -1,14 +1,42 @@
 import GeraCPF from './modules/GeraCFP';
 import './assets/css/style.css';
+import ValidaCPF from './modules/ValidaCPF';
+
+/* Minha solução
+function criaResultado(div) {
+    const stylePag = getComputedStyle(document.body);
+    const background = stylePag.backgroundColor;
+    div.style.textAlign = 'center';
+    div.style.backgroundColor = background;
+    div.style.color = 'white';
+    return div;
+}
 
 document.addEventListener('click', (e) => {
+    e.preventDefault();
     const el = e.target;
-    console.log('chegui aqui')
     
     if (el.classList.contains('button')) {
         const gerarCPF = new GeraCPF();
         const cpf = gerarCPF.geraNovoCpf();
-        const div = document.createElement('div');
+        const validaCpf = new ValidaCPF(cpf);
+        try {
+            validaCpf.valida();
+        } catch (error) {
+            console.log(error);
+        }
+        let div = document.createElement('div');
         div.innerHTML = cpf;
+        div = criaResultado(div);
+        const resultado = document.querySelector('.container');
+        resultado.appendChild(div);
     }
 });
+*/
+
+// solução professor
+(function() {
+    const gera = new GeraCPF();
+    const cpfGerado = document.querySelector('.cpf-gerado');
+    cpfGerado.innerHTML = gera.geraNovoCpf();
+})();
--- javascriptToolingModules/gerador-cpf/webpack/src/modules/GeraCFP.js
+++ javascriptToolingModules/gerador-cpf/webpack/src/modules/GeraCFP.js
@@ -1,14 +1,24 @@
-import cpf from './Cpf'
+import ValidaCPF from './ValidaCPF'
 
 export default class GeraCFP {
     rand(min = 100000000, max = 999999999) {
         return String(Math.floor(Math.random() * (max - min) + min));
     }
 
+    formata(cpf) {
+        return (
+            cpf.slice(0, 3) + '.' +
+            cpf.slice(3, 6) + '.' +
+            cpf.slice(6, 9) + '-' +
+            cpf.slice(9, 11)
+        );
+    }
+
     geraNovoCpf() {
         const cpfSemDigito = this.rand();
         const digito1 = ValidaCPF.geraDigito(cpfSemDigito);
         const digito2 = ValidaCPF.geraDigito(cpfSemDigito + digito1);
-        return cpfSemDigito + digito1 + digito2;
+        const cpfNovo = cpfSemDigito + digito1 + digito2;
+        return this.formata(cpfNovo);
     }
 }
