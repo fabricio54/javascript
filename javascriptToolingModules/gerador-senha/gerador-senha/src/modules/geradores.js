7af225f20305410778ac602246099e02c7484636
Fabricio Alves <fabricioallves2020@gmail.com>
2023-05-19 14:53:46 -0300

    update

---
+++ javascriptToolingModules/gerador-senha/gerador-senha/src/modules/geradores.js
@@ -0,0 +1,37 @@
+// utilizando uma função interna do js para gerar caracteres
+const rand = (min, max) => Math.floor(Math.random() * (max - min) + max);
+
+const geraMaiuscula = () => String.fromCharCode(rand(65, 91));
+
+const geraMinuscula = () => String.fromCharCode(rand(97, 123))
+
+const geraNumero = () => String.fromCharCode(48, 58);
+
+const simbolo = ',.;~^[]{}!@#$%*()_+=-';
+
+const geraSimbolo = () => simbolo[rand(0, simbolo.length)];
+
+// exportando a função para fora do módulo
+
+export default function geraSenha(qtd, maiusculas, minusculas, numeros, simbolos) {
+    const senhaArray = [];
+    qtd = Number(qtd);
+
+    for(let i = 0; i < qtd; i++) {
+        // avaliação de curto circuito
+
+        // se a primeira avaliação for verdadeira o código executará (a segunda automaticamente será verdadeira)
+
+        maiusculas && senhaArray.push(geraMaiuscula());
+
+        minusculas && senhaArray.push(geraMinuscula());
+
+        numeros && senhaArray.push(geraNumero());
+
+        simbolos && senhaArray.push(geraSimbolo());
+
+    }
+    return senhaArray.join('').slice(0, qtd);
+}
+
+geraSenha(8, true, true, true);
